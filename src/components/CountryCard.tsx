fecf01c14ea4e50f72760329dc731a1a4e69f01f
nykw <nykw196884@gmail.com>
2021-04-01 10:17:35 +0900

    step2

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+// import Flag from 'react-flags';
+const Flag = require('react-flags');
+
+type Props = {
+  country: {
+    cca2: string;
+    region: string;
+    name: {
+      common: string;
+    };
+  };
+};
+
+const CountryCard: FC<Props> = ({ country }) => {
+  const { cca2: code2 = '', region = null, name } = country || {};
+
+  return (
+    <div className="col-sm-6 col-md-4 country-card">
+      <div className="country-card-container border-gray rounded border mx-2 my-3 d-flex flex-row align-items-center p-0 bg-light">
+        <div className="h-100 position-relative border-gray border-right px-2 bg-white rounded-left">
+          <Flag
+            country={code2}
+            format="png"
+            pngSize={64}
+            basePath="./img/flags"
+            className="d-block h-100"
+          />
+        </div>
+        <div className="px-3">
+          <span className="country-name text-dark d-block font-weight-bold">{name.common}</span>
+          <span className="country-region text-secondary text-uppercase">{region}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
